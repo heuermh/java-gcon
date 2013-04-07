a690059b4021325c724d033f91835a1214453f2e
Michael Heuer <heuermh@acm.org>
2013-04-07 16:47:41 -0500

    starting to add jclouds implementation

---
+++ gcon-jclouds/src/main/java/com/github/heuermh/gcon/jclouds/BlobStoreGenomeConnector.java
@@ -0,0 +1,40 @@
+/*
+
+    java-gcon  Genome connector APIs.
+    Copyright (c) 2013 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package com.github.heuermh.gcon.jclouds;
+
+import java.util.Map;
+
+import com.github.heuermh.gcon.GenomeConnector;
+import com.github.heuermh.gcon.GenomeConnectorClient;
+
+/**
+ * jclouds BlobStore-based implementation of the genome connector APIs.
+ */
+final class BlobStoreGenomeConnector implements GenomeConnector {
+
+    @Override
+    public GenomeConnectorClient createClient(final Map<String, String> context) {
+        return new BlobStoreGenomeConnectorClient(context);
+    }
+}
---
+++ gcon-jclouds/src/main/java/com/github/heuermh/gcon/jclouds/BlobStoreGenomeConnectorClient.java
@@ -0,0 +1,96 @@
+/*
+
+    java-gcon  Genome connector APIs.
+    Copyright (c) 2013 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package com.github.heuermh.gcon.jclouds;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Map;
+
+import com.github.heuermh.gcon.GenomeConnectorClient;
+import com.github.heuermh.gcon.GenomeConnectorFile;
+import com.github.heuermh.gcon.GenomeConnectorFileMetadata;
+import com.github.heuermh.gcon.GenomeConnectorFileSet;
+import com.github.heuermh.gcon.GenomeConnectorClient;
+
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.BlobStoreContextFactory;
+
+/**
+ * jclouds BlobStore-based implementation of a genome connector client.
+ */
+final class BlobStoreGenomeConnectorClient implements GenomeConnectorClient {
+    private final String container;
+    private final String metadataContainer;
+    private final BlobStoreContext blobStoreContext;
+
+    BlobStoreGenomeConnectorClient(final Map<String, String> context) {
+        checkNotNull(context);
+        container = "container";
+        metadataContainer = "metadataContainer";
+        /*
+           context = ContextBuilder.newBuilder("aws-s3")
+                         .credentials(apikey, secret)
+                         .buildView(BlobStoreContext.class);
+         */
+        String identity = "identity";
+        String credential = "credential";
+        blobStoreContext = new BlobStoreContextFactory().createContext("aws-s3", identity, credential);
+    }
+
+
+    @Override
+    public Iterable<GenomeConnectorFileSet> list() {
+        return null;
+    }
+
+    @Override
+    public Iterable<GenomeConnectorFile> list(final GenomeConnectorFileSet fileSet)  {
+        checkNotNull(fileSet);
+        return null;
+    }
+
+    @Override
+    public GenomeConnectorFileMetadata meta(final GenomeConnectorFile file) {
+        checkNotNull(file);
+        return null;
+    }
+
+    @Override
+    public InputStream get(final GenomeConnectorFile file) {
+        checkNotNull(file);
+        return null;
+    }
+
+    @Override
+    public void get(final GenomeConnectorFile file, final Path path) {
+        checkNotNull(file);
+        checkNotNull(path);
+    }
+
+    @Override
+    public void put() {
+    }
+}
