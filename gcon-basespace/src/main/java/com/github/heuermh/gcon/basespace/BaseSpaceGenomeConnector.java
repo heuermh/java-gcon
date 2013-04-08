121ddd6266cf14d56326322b1600e5ef130a935e
Michael Heuer <heuermh@acm.org>
2013-04-07 20:56:48 -0500

    adding basespace implementation

---
+++ gcon-basespace/src/main/java/com/github/heuermh/gcon/basespace/BaseSpaceGenomeConnector.java
@@ -0,0 +1,99 @@
+/*
+
+    java-gcon  Genome connector APIs.
+    Copyright (c) 2013 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package com.github.heuermh.gcon.basespace;
+
+import java.util.Map;
+
+import com.github.heuermh.gcon.GenomeConnector;
+import com.github.heuermh.gcon.GenomeConnectorClient;
+
+import com.illumina.basespace.BaseSpaceConfiguration;
+import com.illumina.basespace.BaseSpaceSession;
+import com.illumina.basespace.BaseSpaceSessionManager;
+
+/**
+ * BaseSpace implementation of the genome connector APIs.
+ */
+final class BaseSpaceGenomeConnector implements GenomeConnector {
+
+    @Override
+    public final GenomeConnectorClient createClient(final Map<String, String> context) {
+        BaseSpaceConfiguration config = new BaseSpaceConfiguration() {
+                // todo:  use values from context
+
+                @Override
+                public String getVersion() {
+                    return "v1pre3"; // The current version of the API
+                }
+
+                @Override
+                public String getApiRootUri() {
+                    return "https://api.basespace.illumina.com"; // The API server's URI
+                }
+
+                @Override
+                public String getAccessTokenUriFragment() {
+                    return "/oauthv2/token"; // The API call to request an Access Token
+                }
+
+                @Override
+                public String getAuthorizationUriFragment() {
+                    return "/oauthv2/deviceauthorization";  // The API call to request a device authorization code
+                }
+
+                @Override
+                public String getAuthorizationScope() {
+                    return "browse global"; // The scope specified earlier
+                }
+
+                @Override
+                public String getProxyHost() {
+                    return null;
+                }
+
+                @Override
+                public int getProxyPort() {
+                    return 0;
+                }
+
+                @Override
+                public String getAccessToken() {
+                    return null; // This token is null for apps that do not have a permanent access token
+                }
+
+                @Override
+                public String getClientId() {
+                    return "client id"; // Our client id from My Apps
+                }
+
+                @Override
+                public String getClientSecret() {
+                    return "client secret"; // Our client secret from My Apps
+                }
+            };
+
+        BaseSpaceSession session = BaseSpaceSessionManager.instance().requestSession(config);
+        return new BaseSpaceGenomeConnectorClient(session);
+    }
+}
---
+++ gcon-basespace/src/main/java/com/github/heuermh/gcon/basespace/BaseSpaceGenomeConnectorClient.java
@@ -0,0 +1,110 @@
+/*
+
+    java-gcon  Genome connector APIs.
+    Copyright (c) 2013 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package com.github.heuermh.gcon.basespace;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Files;
+
+import com.github.heuermh.gcon.GenomeConnectorClient;
+import com.github.heuermh.gcon.GenomeConnectorFile;
+import com.github.heuermh.gcon.GenomeConnectorFileMetadata;
+import com.github.heuermh.gcon.GenomeConnectorFileSet;
+import com.github.heuermh.gcon.GenomeConnectorClient;
+
+import com.illumina.basespace.BaseSpaceSession;
+
+/**
+ * BaseSpace implementation of a genome connector client.
+ */
+final class BaseSpaceGenomeConnectorClient implements GenomeConnectorClient {
+    private final BaseSpaceSession session;
+
+    //@Inject
+    BaseSpaceGenomeConnectorClient(final BaseSpaceSession session) {
+        checkNotNull(session);
+        this.session = session;
+    }
+
+
+    @Override
+    public Iterable<GenomeConnectorFileSet> list() {
+        return ImmutableList.of();
+    }
+
+    @Override
+    public Iterable<GenomeConnectorFile> list(final GenomeConnectorFileSet fileSet)  {
+        checkNotNull(fileSet);
+        return ImmutableList.of();
+    }
+
+    @Override
+    public GenomeConnectorFileMetadata meta(final GenomeConnectorFile file) {
+        checkNotNull(file);
+        return null;
+    }
+
+    @Override
+    public InputStream get(final GenomeConnectorFile file) {
+        checkNotNull(file);
+
+        // todo: getFile is keyed by id, not uri
+        return session.getFileInputStream(session.getFile(file.toString()));
+    }
+
+    @Override
+    public void get(final GenomeConnectorFile file, final Path path) {
+        checkNotNull(file);
+        checkNotNull(path);
+
+        InputStream inputStream = null;
+        try {
+            inputStream = get(file);
+
+            // write as byte stream
+            Files.asByteSink(path.toFile()).writeFrom(inputStream);
+        }
+        catch (IOException e) {
+            // rethrow?
+        }
+        finally {
+            try {
+                inputStream.close();
+            }
+            catch (IOException e) {
+                // ignore
+            }
+        }
+    }
+
+    @Override
+    public void put() {
+    }
+}
